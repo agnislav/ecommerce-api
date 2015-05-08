f8fb6200361ecdda1c10be923d8aa0c3aea2c813
Agnislav Onufrijchuk <agnislav@gmail.com>
2015-05-08 03:49:21 +0300

    Code structure improved

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -2,7 +2,9 @@
 
 var express   = require('express');
 var router    = express.Router();
-var dbManager = require('../lib/dbManager');
+//var dbManager = require('../lib/dbManager');
+var productManager = require('../lib/productManager');
+//var categoryManager = require('../lib/categoryManager');
 
 /* GET index. */
 router.get('/', function (req, res) {
@@ -11,14 +13,21 @@ router.get('/', function (req, res) {
 
 /* GET product by id. */
 router.get('/product/:id', function (req, res) {
-  var productID = Number(req.params.id),
-      product, err;
+  const productID = req.params.id;
 
-  dbManager.getProductByID(productID, function (err, result) {
-    if (err === null) {
-      res.json(result);
-    }
-  });
+  if (productID !== null && productID !== undefined) {
+    productManager.getProductByID(productID, { cb: function (err, result) { // todo: move this function out
+      if (err === null) {
+        res.json(result);
+      }
+      else {
+        res.json({message: err}); // todo: specify standard error object
+      }
+    }});
+  }
+  else {
+    res.json({message: "product id isn't specified"}); // todo: specify standard error object
+  }
 });
 
 module.exports = router;
